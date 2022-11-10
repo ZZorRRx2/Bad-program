9dbbdec2115853f485e637ee2954f84bca1e05c3
ZZorRRx2 <falconway4@gmail.com>
2022-11-10 20:59:15 +0000

    Don't even ask
    I just added more numbers to the code so that more people can die inside reading this code

---
+++ odd or even.py
@@ -0,0 +1,29 @@
+number = int(input("please enter a number"))
+if number == 1:
+    print("odd")
+elif number == 2:
+    print("even")
+elif number == 3:
+    print("odd")
+elif number == 4:
+    print("even")
+elif number == 5:
+    print("odd")
+elif number == 6:
+    print("even")
+elif number == 7:
+    print("odd")
+elif number == 8:
+    print("even")
+elif number == 9:
+    print("odd")
+elif number == 10:
+    print("even")
+elif number ==11:
+    print("odd")
+elif number == 12:
+    print("even")
+elif number == 13:
+    print("odd")
+else:
+    print("not implemented")
